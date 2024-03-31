8570f3df97989bba5c1065d12ef49822f869305c
joaco <joaco@LAPTOP-G9SRR02E>
2024-03-31 17:09:00 -0300

    intentar conexion

---
+++ Papeleria/Controllers/UsuarioController.cs
@@ -0,0 +1,94 @@
+﻿using Papeleria.LogicaNegocio.Entidades;
+using Papeleria.LogicaNegocio.InterfacesAccesoDatos;
+using Papeleria.LogicaNegocio.ValueObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Papeleria.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IRepositorioUsuario repositorioUsuario { get; set; }
+        public UsuarioController(IRepositorioUsuario repositorioUsuario) 
+        {
+            this.repositorioUsuario = repositorioUsuario;
+        }
+        // GET: UsuarioController
+        public ActionResult Index()
+        {
+            return View(repositorioUsuario.FindAll());
+        }
+
+        // GET: UsuarioController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuarioController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Usuario usuario, String nombre, String apellido, String email)
+        {
+            try
+            {
+                NombreCompleto nombreUsuario = new NombreCompleto(nombre, apellido);
+                usuario.NombreCompleto = nombreUsuario;
+                repositorioUsuario.Add(usuario);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
